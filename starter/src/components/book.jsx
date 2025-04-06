b39985f20aacef89cbf08617897ec066b7c28a3d
Xiao Fang <ganxiaofang@gmail.com>
2025-04-05 17:54:07 -0700

    fix none option

--- starter/src/components/book.jsx
+++ starter/src/components/book.jsx
@@ -18,7 +18,7 @@ export const Book = ({ onChange, book }) => {
       ></div>
       <div className="book-shelf-changer">
         <select value={book.shelf} onChange={handleChange}>
-          <option value="none" disabled>Move to...</option>
+          <option disabled>Move to...</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
           <option value="read">Read</option>
